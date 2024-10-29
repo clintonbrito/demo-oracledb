a05f33b01a856e02d845d233b421be542e137a67
Clinton Brito <clintoncsb@gmail.com>
2024-10-29 13:10:12 -0300

    first commit

---
+++ src/test/java/com/clinton/demo_oracledb/DemoOracledbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clinton.demo_oracledb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoOracledbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
